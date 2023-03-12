40c49d2cfb60e20a3770e4d7222abb19fb7699da
abirahmed56 <abirahmedsohan6@gmail.com>
2023-03-12 17:49:35 +0600

    og image added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,26 @@
 import type { AppProps } from 'next/app'
-import nextSeoConfig from '../next-seo.config';
 import { DefaultSeo } from 'next-seo';
 
 export default function App({ Component, pageProps, }: AppProps) {
   return (
-    <>
+    <> 
         <DefaultSeo
-            title='Blog Application'
+            title='Blog Application' 
             description='This is a Blog Application'
+            openGraph={{
+                url: 'https://www.shutterstock.com',
+                title: 'Blog_Application',
+                description: 'Write and Read Blogs',
+                images:[
+                    {
+                        url: 'https://www.shutterstock.com/image-photo/working-home-laptop-woman-writing-blog-348905468',
+                        alt:'Blogs',
+                        type:'image/jpeg'
+                    },
+                ],
+                site_name: 'Blog_Application'
+            }
+            }
         />
         <Component {...pageProps} />
     </>
--- pages/admin.tsx
+++ pages/admin.tsx
@@ -3,17 +3,12 @@ import Admin from "../components/blogs/AdminContent";
 import Create from "../modal/Create";
 import { NextSeo } from "next-seo";
 
-
-
-
-
 function admin() {
   return (
     <>
       <NextSeo
             title="Admin"
             description="This is admin page of Blog Application"
-            
             
         />
      
